ebf156f2850972d8c11aefe5610fc598ac38a867
ultrAlper23 <alper.kalaycioglu@gmail.com>
2016-07-18 14:53:19 +0300

    First Commit at 2016.07.18

---
+++ MyFirstRepository/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstRepository
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*This is my first repo on GitHub*/
+        }
+    }
+}
